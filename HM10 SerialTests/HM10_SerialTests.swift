a5f3934aaef7388f69f64b53039ba33bee968354
hoiberg <alex@Marinuss-iMac.fritz.box>
2015-08-09 17:02:41 +0200

    Initial Commit

---
+++ HM10 SerialTests/HM10_SerialTests.swift
@@ -0,0 +1,36 @@
+//
+//  HM10_SerialTests.swift
+//  HM10 SerialTests
+//
+//  Created by Alex on 09-08-15.
+//  Copyright (c) 2015 Balancing Rock. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HM10_SerialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
